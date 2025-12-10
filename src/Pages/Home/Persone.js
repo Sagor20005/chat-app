b4ee9ba7c53e7a048176729a1fb44df485a4c2f0
Sagor20005 <ms6392883@gmail.com>
2025-12-10 10:32:44 +0600

    complite typing indicate on chatlist

--- src/Pages/Home/Persone.js
+++ src/Pages/Home/Persone.js
@@ -8,7 +8,9 @@ export default function Persone({ last_message, chat }){
   const { _id } = useParsedCookie()
   const { user_avatar, user_name, typing } = chat
   const isMyMessage = last_message.sender === _id
-  const last_message_text = last_message.text ? last_message.text.slice(0,20)+ last_message.text.length>20 && "..." : "Start new chat."
+  const last_message_text = last_message.text ? last_message.text.slice(0,20) : "Start new chat."
+  
+  console.log(last_message_text)
   
   return(
     <div onClick={()=>Navigate("/inbox",{ state:{chat} })} className="hover:scale-95 transition flex justify-between items-center p-3" key={chat.user_name}>
@@ -16,7 +18,7 @@ export default function Persone({ last_message, chat }){
           <img className="w-[70px] h-[70px] rounded-full shrink-0 object-cover" src={user_avatar ? user_avatar : defaultAvatar} alt="Persone" />
           <div className="flex flex-col ">
             <p>{user_name}</p>
-            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{ typing === true ? "Typing..." : last_message_text }</p>
+            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{typing || last_message_text }</p>
           </div>
         </div>
         {
