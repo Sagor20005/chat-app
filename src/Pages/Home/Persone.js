1ff9249bc15a00c4195256f538fc2bfe4056f3a9
Sagor20005 <ms6392883@gmail.com>
2025-12-10 08:38:06 +0600

    add typing indicator on chatlist

--- src/Pages/Home/Persone.js
+++ src/Pages/Home/Persone.js
@@ -6,9 +6,9 @@ export default function Persone({ last_message, chat }){
   const defaultAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7aH70ubSk8FPfa1NLXvIP_wWOVbueWEQkA&usqp=CAU"
   
   const { _id } = useParsedCookie()
-  const { user_avatar, user_name } = chat
+  const { user_avatar, user_name, typing } = chat
   const isMyMessage = last_message.sender === _id
-  const last_message_text = last_message.text ? last_message.text.slice(0,20)+"..." : "Start new chat."
+  const last_message_text = last_message.text ? last_message.text.slice(0,20)+ last_message.text.length>20 && "..." : "Start new chat."
   
   return(
     <div onClick={()=>Navigate("/inbox",{ state:{chat} })} className="hover:scale-95 transition flex justify-between items-center p-3" key={chat.user_name}>
@@ -16,7 +16,7 @@ export default function Persone({ last_message, chat }){
           <img className="w-[70px] h-[70px] rounded-full shrink-0 object-cover" src={user_avatar ? user_avatar : defaultAvatar} alt="Persone" />
           <div className="flex flex-col ">
             <p>{user_name}</p>
-            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{last_message_text}</p>
+            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{ typing ? "Typing..." : last_message_text }</p>
           </div>
         </div>
         {
