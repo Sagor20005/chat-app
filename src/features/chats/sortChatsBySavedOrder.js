b0691310b9afb4fb990b8e787db204a7d473dfc9
Sagor20005 <ms6392883@gmail.com>
2026-01-20 22:39:57 +0600

    fix empty null chat list when server not working

--- src/features/chats/sortChatsBySavedOrder.js
+++ src/features/chats/sortChatsBySavedOrder.js
@@ -1,7 +1,7 @@
 function sortChatBySAvedOrder(Chats){
     const LSorder = window.localStorage.getItem("jchat-order")
     const order = LSorder ? ( JSON.parse(LSorder) ) : null
-    if(order && order.length){
+    if(order && (order.length === Chats.length) && order.length){
         const OrderdChat = []
         for (const chat_id of order){
             const chat = Chats.find( c => c.chat_id === chat_id )
